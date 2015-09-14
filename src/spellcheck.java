47b00c5685c62f947132308be3162f3fbccef530
cs12b015 <jayanthk@cse.iitm.ac.in>
2015-09-15 02:56:59 +0530

    first edge completed

--- src/spellcheck.java
+++ src/spellcheck.java
@@ -21,12 +21,68 @@ public class spellcheck {
 	    	System.out.println("yes it is a correct word");
 	    }
 	    else{
-	    	System.out.println("no it is a wrong word");
+	    	/*System.out.println("no it is a wrong word");*/
+	    	List <String> editings = editings(args[0]);
+	    	List<String> correctwords = new ArrayList<String>();
+	    	for(int i=0;i<editings.size();i++){
+	    		if(checkspelling(editings.get(i))){
+	    			correctwords.add(editings.get(i));
+	    		}
+	    	}
+	    	System.out.println(correctwords);
+	    	
 	    }	
-	    
-		
-		
 	}	
+	
+	public static Boolean checkspelling(String word){
+		if(dictionary.containsKey(word.toUpperCase()))
+	    {
+	    	return true;
+	    }
+		else
+		  return false;		
+	}
+		
+	public static List<String> editings(String word) {
+		List<String> list = new ArrayList<String>();
+		
+		/*Deletions*/
+		int wordlength=word.length();
+		for(int i=0;i<wordlength;i++){
+			String temp = word.substring(0,i)+word.substring(i+1,wordlength) ;
+			list.add(temp);	
+		}
+		
+		/*replacing a letter*/
+		for(int i=0;i<wordlength;i++){
+			for(char j='a';j<'z';j++){
+				String temp = word.substring(0,i)+j+word.substring(i+1,wordlength) ;
+				list.add(temp);	
+			}
+		}
+		/*insertion of one letter*/
+		for(int i=0;i<wordlength+1;i++){
+			for(char j='a';j<'z';j++){
+				String temp = word.substring(0,i)+j+word.substring(i,wordlength) ;
+				list.add(temp);	
+			}
+		}
+		/*transpose of adjacent symbols */
+		for(int i=0;i<wordlength-1;i++){
+			
+			char a = word.charAt(i);
+			char b = word.charAt(i+1);
+			
+			String temp = word.substring(0,i)+b+word.substring(i+1);
+			temp = temp.substring(0,i+1)+a+temp.substring(i+2);
+				
+			list.add(temp);
+		}
+			
+		
+		return list;
+	}
+	
 	
 	
 	
